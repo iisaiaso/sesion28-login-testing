e85ec3104dd54f5682f9816fed614ed78af1bd5e
iisaiaso <isaiasibarraonofre@gmail.com>
2023-01-27 16:26:50 -0500

    Testing completado y estilos agregado

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,6 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
@@ -11,6 +13,9 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      schemas: [
+        NO_ERRORS_SCHEMA
+      ]
     }).compileComponents();
   });
 
@@ -20,16 +25,79 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'sesion28-login-testing'`, () => {
+  it(`should have as title 'Login'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('sesion28-login-testing');
+    expect(app.title).toEqual('Login');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('sesion28-login-testing app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Login');
   });
+
+  /**   Testing de formulario valido */
+  it('Debe retornar formulario valido', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    fixture.detectChanges()
+    let username = app.formLogin.controls['username']
+    let password = app.formLogin.controls['password']
+    username.setValue('isaias@gmail.com'),
+      password.setValue('12345')
+    expect(app.formLogin.invalid).toBeFalsy();
+  });
+
+  it('Debe retornar formulario invalido', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    fixture.detectChanges()
+    let username = app.formLogin.controls['username']
+    let password = app.formLogin.controls['password']
+    username.setValue('isaiasgmail.com'),
+      password.setValue('12345')
+    expect(app.formLogin.invalid).toBeTruthy();
+  });
+
+  /** fin te testing */
+
+  /** Testing de iniciciar seion mediante un click*/
+
+  it(`Debe retornar iniciar sesion`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    fixture.detectChanges()
+
+    let username = app.formLogin.controls['username']
+    let password = app.formLogin.controls['password']
+
+    username.setValue('isaias@gmail.com')
+    password.setValue('12345')
+
+    const btnElement = fixture.debugElement.query(By.css('button.btn'))
+    btnElement.nativeElement.click()
+    const testData = { user: "Estas Logeado" }
+    expect(app.result).toEqual(testData.user)
+  });
+
+  /** */
+
+  it(`Debe retornar no poder ingresar`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    fixture.detectChanges()
+    let username = app.formLogin.controls['username']
+    let password = app.formLogin.controls['password']
+
+    username.setValue('isaiasgmail.com')
+    password.setValue('1234')
+
+    const btnElement = fixture.debugElement.query(By.css('button.btn'))
+    btnElement.nativeElement.click()
+    const testData = { user: "" }
+    expect(app.result).toEqual(testData.user)
+  });
+/** fin Testing */
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy } from '@angular/compiler';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -7,26 +8,30 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'sesion28-login-testing';
+  title = 'Login';
   // username!: string
   // password!: string
   formLogin!: FormGroup
-  result!:string
+  result!: string
+  alert = false
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private cd: ChangeDetectorRef) { }
 
   ngOnInit(): void {
     this.formLogin = this.formBuilder.group({
-      username: ['', [Validators.required, Validators.minLength(5)]],
+      username: ['', [Validators.required, Validators.minLength(5),Validators.email]],
       password: ['', [Validators.required, Validators.minLength(5)]]
     })
+    this.cd.detectChanges()
   }
 
-  onLogin(){
-    if(this.formLogin.valid == true){
-    this.result = 'Estas Logeado'
-    }else{
-      this.result= ""
+  onLogin():any {
+    if (this.formLogin.valid == true) {
+      this.result = 'Estas Logeado'
+      this.alert=true
+    } else {
+      this.result = ""
+      this.alert=false
     }
   }
 

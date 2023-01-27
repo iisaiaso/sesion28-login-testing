08eb782015901146cf4192a67798600d08d27517
iisaiaso <isaiasibarraonofre@gmail.com>
2023-01-27 09:42:09 -0500

    Formulario login concluido

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'sesion28-login-testing';
+  // username!: string
+  // password!: string
+  formLogin!: FormGroup
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.formLogin = this.formBuilder.group({
+      username: ['', [Validators.required, Validators.minLength(5)]],
+      password: ['', [Validators.required, Validators.minLength(5)]]
+    })
+  }
+
 }
